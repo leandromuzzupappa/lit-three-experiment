baae39aa71ce5a81981d967091bbe729f59da9c3
LMuzzupappa <leandro.muzzupappa@mediamonks.com>
2023-07-20 12:18:47 -0300

    feat(ThreeExperience): create simple example

--- src/components/three-experience/ThreeExperience.ts
+++ src/components/three-experience/ThreeExperience.ts
@@ -6,8 +6,12 @@ import {
   Mesh,
   AmbientLight,
   MeshStandardMaterial,
+  Vector3,
+  Group,
+  PointLight,
 } from "three";
 import { LitThree } from "../../utils/LitThree";
+import { UpdateLoopType } from "../../data/types/LitThreeTypes";
 
 @customElement("three-experience")
 export class ThreeExperience extends LitThree {
@@ -15,12 +19,15 @@ export class ThreeExperience extends LitThree {
 
   constructor() {
     super(null);
-  }
 
-  connectedCallback() {
-    super.connectedCallback();
     this.setCanvas();
   }
+
+  wireframeSphereGroup: Group = new Group();
+  wireframedSphere: Mesh | null = null;
+  filledSphere: Mesh | null = null;
+
+  pointLight: PointLight | null = null;
 
   async setCanvas() {
     await this.updateComplete;
@@ -29,14 +36,82 @@ export class ThreeExperience extends LitThree {
     this.setRenderer(this.shadowRoot);
   }
 
-  mainLoop() {
-    const geometry = new SphereGeometry(2, 8, 8);
+  addSphereWireframe() {
+    const geometry = new SphereGeometry(2, 50, 30);
     const material = new MeshBasicMaterial({
-      color: 0xffff00,
+      color: 0xffff00ff,
       wireframe: true,
+      transparent: true,
+      opacity: 0.1,
     });
-    const sphere = new Mesh(geometry, material);
-    this.scene.add(sphere);
+    this.wireframedSphere = new Mesh(geometry, material);
+
+    this.wireframeSphereGroup.add(this.wireframedSphere);
+    this.placeSpheresOnWireframedSphere();
+
+    this.scene.add(this.wireframeSphereGroup);
+  }
+
+  placeSpheresOnWireframedSphere() {
+    const vertices: Vector3[] = [];
+    const positionAttribute =
+      this.wireframedSphere?.geometry.attributes.position;
+    const arr = positionAttribute!.array;
+
+    for (let i = 0; i < arr.length; i += 3) {
+      const x = arr[i];
+      const y = arr[i + 1];
+      const z = arr[i + 2];
+
+      vertices.push(new Vector3(x, y, z));
+    }
+
+    const geometry = new SphereGeometry(0.05);
+    const material = new MeshStandardMaterial({
+      color: "#ff0fff",
+      opacity: 0.9,
+      transparent: true,
+    });
+
+    vertices.forEach((vertex) => {
+      const box = new Mesh(geometry, material);
+
+      box.position.copy(vertex);
+      this.wireframeSphereGroup.add(box);
+    });
+  }
+
+  addSphereFilled() {
+    const geometry = new SphereGeometry(1.7);
+    const material = new MeshStandardMaterial({
+      color: "#333",
+    });
+    this.filledSphere = new Mesh(geometry, material);
+    this.scene.add(this.filledSphere);
+  }
+
+  mainLoop() {
+    if (this.scene.children.length) return;
+
+    const ambientLight = new AmbientLight(0xffffff, 0.3);
+    this.scene.add(ambientLight);
+
+    this.pointLight = new PointLight(0xffffff, 1.2);
+    this.scene.add(this.pointLight);
+
+    this.addSphereWireframe();
+    this.addSphereFilled();
+  }
+
+  updateLoop({ deltaTime }: UpdateLoopType) {
+    this.wireframeSphereGroup?.rotateY(Math.sin(deltaTime * 0.5));
+    this.wireframeSphereGroup?.rotateZ(deltaTime * 0.007);
+
+    this.pointLight?.position.set(
+      Math.sin(deltaTime * 0.5) * 5,
+      Math.cos(deltaTime * 0.5) * 5,
+      Math.cos(deltaTime * 0.5) * 5
+    );
   }
 }
 
