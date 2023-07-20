1e8e26dd087fe36e296d48f1cd2ee543b4f1f158
LMuzzupappa <leandro.muzzupappa@mediamonks.com>
2023-07-20 12:17:05 -0300

    feat(LitThree): create time variables

--- src/utils/LitThree.ts
+++ src/utils/LitThree.ts
@@ -1,13 +1,19 @@
 import { html, LitElement } from "lit";
+import { state } from "lit/decorators.js";
 import { PerspectiveCamera, Scene, WebGLRenderer } from "three";
 // @ts-ignore
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { UpdateLoopType } from "../data/types/LitThreeTypes";
 
 export abstract class LitThree extends LitElement {
   scene!: Scene;
   camera!: PerspectiveCamera;
   renderer!: WebGLRenderer;
   targetElement: ShadowRoot | null = null;
+
+  @state() protected startTime: number = 0;
+  @state() protected deltaTime: number = 0;
+  @state() protected currentTime: number = 0;
 
   constructor(shadowRoot: ShadowRoot | null) {
     super();
@@ -37,27 +43,36 @@ export abstract class LitThree extends LitElement {
     this.renderer.setSize(window.innerWidth, window.innerHeight);
     this.renderer.setPixelRatio(window.devicePixelRatio);
     this.targetElement?.appendChild(this.renderer.domElement);
-
-    console.log(1, this.targetElement);
   }
 
-  setRenderer(target: ShadowRoot | null) {
+  protected setRenderer(target: ShadowRoot | null) {
     target?.appendChild(this.renderer.domElement);
   }
 
   private loop() {
     requestAnimationFrame(this.loop.bind(this));
 
+    this.currentTime = performance.now();
+    this.deltaTime = (this.currentTime - this.startTime) * 0.001;
+
     this.mainLoop();
+    this.updateLoop({
+      deltaTime: this.deltaTime,
+      currentTime: this.currentTime,
+    });
     this.renderer.render(this.scene, this.camera);
   }
 
-  addControls() {
+  protected addControls() {
     const controls = new OrbitControls(this.camera, this.renderer.domElement);
     controls.update();
   }
 
-  abstract mainLoop(): void;
+  protected abstract mainLoop(): void;
+  protected abstract updateLoop({
+    deltaTime,
+    currentTime,
+  }: UpdateLoopType): void;
 
   render() {
     return html`<slot></slot>`;
